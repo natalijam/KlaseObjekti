107d59fad8ebb87f4e8425486eab4116e8c046fb
Natalija <natalija.mitkovic1@gmail.com>
2021-11-28 15:28:02 +0100

    Added examples in Main Class from Krug Class - final and static

--- src/main/java/main/klaseobjekti/Main.java
+++ src/main/java/main/klaseobjekti/Main.java
@@ -5,28 +5,29 @@ public class Main {
 
     
     public static void main(String[] args) {
+
  
-// LEKCIJA KLASE
         
-//  II KORAK
+// LEKCIJA KLASE (Student klasa)
+ 
+ 
 // KREIRAMO VARIJABLU TIPA STUDENT
 //        Student s1;
-//// KREIRAMO NOVI OBJEKAT STUDENT     
+// KREIRAMO NOVI OBJEKAT STUDENT     
 //        s1 = new Student();
 //        
-//// DODELJUJEMO ATRIBUTE NOVOM OBJEKTU
+// DODELJUJEMO ATRIBUTE NOVOM OBJEKTU
 //        s1.age = 18;
 //        s1.name = "Marko";
 //        s1.surname = "Markovic";
-//   PUŠTAMO METODU NOVOM STUDENTU (ispisuje se navedeno iz introduce metode)     
+// PUŠTAMO METODU NOVOM STUDENTU (ispisuje se navedeno iz introduce metode)     
 //        s1.introduce();
 
 
 
+//   LEKCIJA OBJEKTI  (Student klasa)
 
-
-//  II PRIMER - OBJEKTI LEKCIJA
-
+ 
 //        Student s1 = new Student();
 //        Student s2 = new Student();
 //        
@@ -48,49 +49,81 @@ public class Main {
   
         
         
-// LEKCIJA KONSTRUKTOR PRIMER
+// LEKCIJA KONSTRUKTOR (Car klasa)
 
-        Car car1 = new Car();  
+
+//        Car car1 = new Car();
+
 // Car() je konstruktor. On služi da nam kreira objekat klase Car. Klasa i konstruktor imaju isti naziv.
-
-        car1.brand = "Renault";
-        car1.model = "Clio";
-    
-        Car car2 = new Car();
-    
-        car2.brand = "Audi";
-        car2.model = "A4";
-        
-// Car() je defaultni konstruktor - nema parametre. On služi samo da kreira objekat.
-
-
+//
+//        car1.brand = "Renault";
+//        car1.model = "Clio";
+//    
+//        Car car2 = new Car();
+//    
+//        car2.brand = "Audi";
+//        car2.model = "A4";
+//        
+//Car() je defaultni konstruktor - nema parametre. On služi samo da kreira objekat.
+//
+//
 // Kreiranje objekata sa konstruktorom koji ima parametre
-    Car car3 = new Car("BMW", "520");
-    
+//        Car car3 = new Car("BMW", "520");
 
-// LEKCIJA METODE
 
-    Pravougaonik p1 = new Pravougaonik(4, 12);
-    Pravougaonik p2 = new Pravougaonik(7, 9);
+
+
+//   LEKCIJA METODE (Pravougaonik klasa)
+//
+
+//    Pravougaonik p1 = new Pravougaonik(4, 12);
+//    Pravougaonik p2 = new Pravougaonik(7, 9);
+//    
+//    p1.stampajObim();
+//    p2.stampajObim();
+//    
+//    p1.stampajPovrsinu();
+//    
+//    p1.izracunajObim();
+//    
+// pošto vraća rezultat int, možemo da napravimo novu varijablu sa tom vrednosšću
+//    
+//    int obimP1 = p1.izracunajObim();
+//    System.out.println("Obim objekta p1 je: " + obimP1);
+//    
+//    int povrsinaP1 = p1.izracunajPovrsinu();    
+//    System.out.println("Povrsina objekta p1 je: " + povrsinaP1);
+ 
+
     
-    p1.stampajObim();
-    p2.stampajObim();
-    
-    p1.stampajPovrsinu();
-    
-    p1.izracunajObim();
-    
-    //pošto vraća rezultat int, možemo da napravimo novu varijablu sa tom vrednosšću
-    
-    int obimP1 = p1.izracunajObim();
-    System.out.println("Obim objekta p1 je: " + obimP1);
-    
-    int povrsinaP1 = p1.izracunajPovrsinu();    
-    System.out.println("Povrsina objekta p1 je: " + povrsinaP1);
+    // FINAL  (Krug klasa)
     
     
-            
-            
+//    Krug k1 = new Krug(2.3);
+//    
+//    Krug k2 = new Krug(5.0);
+//    
+//    k1.izracunajObim();
+//    k2.izracunajPovrsinu();
+ 
+
+
+    // STATIC  (Krug klasa)
+    
+    
+//    Krug k1 = new Krug(9.0);    
+//    Krug k2 = new Krug(5.0);
+    
+    //oba objekta sada imaju po dva atributa u svojoj memoriji, ali kada dodelimo static za PI,
+    // onda oni čuvaju po jedan atribut, a atribut PI je sada zajednički i izdvojen u memoriji
+    
+    double pi = Krug.PI;
+    // kada je static field, ne mora da ima objekat da bi mogao da se pozove
+    
+    Krug.izracunajPovrsinu(3.0);
+    // metoda ne mora da ima objekat da bi bila pozvana
+    // dodelili smo vrednost prosleđenom parametru 3.0, ili možemo bilo koju drugu
+    
     }
     
 }
